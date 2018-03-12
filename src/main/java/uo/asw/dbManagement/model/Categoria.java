746f97af9fb762b5f383ed266606ddddec4c9f78
ameliafb <ameliafb@gmail.com>
2018-03-12 20:18:49 +0100

    modelo de datos, version con tipos-continuacion

--- src/main/java/uo/asw/dbManagement/model/Categoria.java
+++ src/main/java/uo/asw/dbManagement/model/Categoria.java
@@ -21,15 +21,83 @@ public class Categoria {
 	
 	@Enumerated(EnumType.STRING) private CategoriaTipos categoria;
 	
-	private Double valor;
-	
 	@Column(name = "id_incidencia")
 	@ManyToOne
 	private Long idIncidenciaC;
 	
 	public Categoria() {}
 
+	public Categoria(CategoriaTipos categoria, Long idIncidenciaC) {
+		super();
+		this.categoria = categoria;
+		this.idIncidenciaC = idIncidenciaC;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public CategoriaTipos getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaTipos categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getIdIncidenciaC() {
+		return idIncidenciaC;
+	}
+
+	public void setIdIncidenciaC(Long idIncidenciaC) {
+		this.idIncidenciaC = idIncidenciaC;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((idIncidenciaC == null) ? 0 : idIncidenciaC.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (categoria != other.categoria)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (idIncidenciaC == null) {
+			if (other.idIncidenciaC != null)
+				return false;
+		} else if (!idIncidenciaC.equals(other.idIncidenciaC))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", categoria=" + categoria 
+				+ ", idIncidenciaC=" + idIncidenciaC + "]";
+	}
+
+	
 	
 	
 
--- src/main/java/uo/asw/dbManagement/model/Propiedad.java
+++ src/main/java/uo/asw/dbManagement/model/Propiedad.java
@@ -25,5 +25,90 @@ public class Propiedad {
 	@ManyToOne
 	private Long idIncidencia;
 	
+	private Double valor;
+	
+	public Propiedad() {}
+
+	public Propiedad(PropiedadTipos propiedad, Long idIncidencia, Double valor) {
+		super();
+		this.propiedad = propiedad;
+		this.idIncidencia = idIncidencia;
+		this.valor = valor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public PropiedadTipos getPropiedad() {
+		return propiedad;
+	}
+
+	public void setPropiedad(PropiedadTipos propiedad) {
+		this.propiedad = propiedad;
+	}
+
+	public Long getIdIncidencia() {
+		return idIncidencia;
+	}
+
+	public void setIdIncidencia(Long idIncidencia) {
+		this.idIncidencia = idIncidencia;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((idIncidencia == null) ? 0 : idIncidencia.hashCode());
+		result = prime * result + ((propiedad == null) ? 0 : propiedad.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Propiedad other = (Propiedad) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (idIncidencia == null) {
+			if (other.idIncidencia != null)
+				return false;
+		} else if (!idIncidencia.equals(other.idIncidencia))
+			return false;
+		if (propiedad != other.propiedad)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Propiedad [id=" + id + ", propiedad=" 
+				+ propiedad + ", idIncidencia=" + idIncidencia + ", valor="
+				+ valor + "]";
+	}
+	
+	
 
 }
