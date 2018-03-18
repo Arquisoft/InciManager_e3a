51e9b6d93df4c91e65eb72e1ee06c595e67f5c50
saulcasti <saul_casti@hotmail.es>
2018-03-18 11:35:10 +0100

    Resuelto problema
    Solicitaba contraseña de localhost al poner la url

---
+++ src/main/java/uo/asw/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package uo.asw;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService; import org.springframework.security.core.userdetails.UsernameNotFoundException; import org.springframework.stereotype.Service;
+
+import uo.asw.dbManagement.model.Agente;
+import uo.asw.inciManager.repository.AgenteRespository;
+
+import java.util.*;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private AgenteRespository agenteRespository;
+
+	@Override
+	public UserDetails loadUserByUsername(String identificador) throws UsernameNotFoundException {
+
+		Agente agente = agenteRespository.findByIdentificador(identificador);
+		if(agente != null){
+			Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+			return new org.springframework.security.core.userdetails.User( 
+					agente.getEmail(), agente.getContrasena(), grantedAuthorities);
+		}throw new UsernameNotFoundException(identificador);
+	}
+
+}
--- src/main/java/uo/asw/inciManager/repository/AgenteRespository.java
+++ src/main/java/uo/asw/inciManager/repository/AgenteRespository.java
@@ -9,4 +9,7 @@ public interface AgenteRespository extends CrudRepository<Agente, Long>{
 	
 	@Query("SELECT a FROM Agente a WHERE a.nombre=?1 and a.contrasena=?2 and a.kindCode=?3")
 	Agente findeAgentByUserPassKind(String user, String password, String kindCode);
+
+	@Query("SELECT a FROM Agente a WHERE a.identificador=?1")
+	Agente findByIdentificador(String identificador);
 }
