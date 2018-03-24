7e1ab458365b5d151cc3944a5c13e291d3eef2ee
Pelayo Díaz Soto <UO251000@uniovi.es>
2018-03-25 00:46:17 +0100

    Casi finalizada la rama de envio de dependencias, pendiente de correcc

--- src/main/java/uo/asw/inciManager/controller/IncidenciasController.java
+++ src/main/java/uo/asw/inciManager/controller/IncidenciasController.java
@@ -1,7 +1,9 @@
 package uo.asw.inciManager.controller;
 
 import java.security.Principal;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,11 +15,14 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 import uo.asw.dbManagement.model.Agente;
 import uo.asw.dbManagement.model.Categoria;
 import uo.asw.dbManagement.model.Incidencia;
+import uo.asw.dbManagement.model.Propiedad;
 import uo.asw.dbManagement.tipos.CategoriaTipos;
+import uo.asw.dbManagement.tipos.PropiedadTipos;
 import uo.asw.iniManager.service.AgenteService;
 import uo.asw.iniManager.service.IncidenciasService;
 
@@ -54,18 +59,47 @@ public class IncidenciasController {
 	}
 	
 	@RequestMapping(value="/incidencia/create", method = RequestMethod.POST)
-	public String createNewIncidence(@Validated Incidencia incidencia, Principal principal,
-			@RequestParam("category") String category /*)@RequestParam("drivinVelocity") String dV,
-			@RequestParam("windVelocity") String wV, @RequestParam("preasure") String p, 
-			@RequestParam("humedad") String h, @RequestParam("temperature") String t*/) {
-
+	public String createNewIncidence(@Validated Incidencia incidencia, Principal principal, 
+			@RequestParam("category") String category, WebRequest webRequest) {
+		
 		Categoria categoria = new Categoria(CategoriaTipos.valueOf(category), incidencia);
 		String emailAgente = principal.getName();
 		Agente agente = agenteService.getAgentByIdentificador(emailAgente);
+		incidencia.setPropiedades(obtainProperties(incidencia, webRequest));
 		
 		incidenciasService.createNewIncidencia(incidencia, categoria, agente);
 		
 		return "redirect:/incidencia/list";
 	}
 
+	/**
+	 * Método que devuelve una coleccion de las propiedades introducidas
+	 * @param incidencia la incidencia
+	 * @param webRequest
+	 * @return set con todas las propiedades introducidas.
+	 */
+	private Set<Propiedad> obtainProperties(Incidencia incidencia, WebRequest webRequest) {
+		Set<Propiedad> propiedades = new HashSet<Propiedad>();
+		
+		String drivinV = webRequest.getParameter("drivinVelocity");
+		String windV = webRequest.getParameter("windVelocity");
+		String preasure = webRequest.getParameter("preasure");
+		String humedad = webRequest.getParameter("humedad");
+		String temperature = webRequest.getParameter("temperature");
+		
+		if (drivinV!=null) {
+			propiedades.add(new Propiedad(PropiedadTipos.valueOf("VELOCIDAD_CIRCULACION"), incidencia, Double.valueOf(drivinV)));
+		} if (windV!=null) {
+			propiedades.add(new Propiedad(PropiedadTipos.valueOf("VELOCIDAD_VIENTO"), incidencia, Double.valueOf(windV)));
+		} if (preasure!=null) {
+			propiedades.add(new Propiedad(PropiedadTipos.valueOf("PRESION"), incidencia, Double.valueOf(preasure)));
+		} if (humedad!=null) {
+			propiedades.add(new Propiedad(PropiedadTipos.valueOf("HUMEDAD"), incidencia, Double.valueOf(humedad)));
+		} if (temperature!=null) {
+			propiedades.add(new Propiedad(PropiedadTipos.valueOf("TEMPERATURA"), incidencia, Double.valueOf(temperature)));
+		}
+		
+		return propiedades;
+	}
+	
 }
--- src/main/resources/static/script/createIncidence.js
+++ src/main/resources/static/script/createIncidence.js
@@ -1,29 +1,23 @@
-function controlador(boolean vCircu, boolean vViento, 
-					boolean presion, boolean humedad, boolean temperatura){
+function comprobador() {
+	if ($("#category option:selected").text() === "Accidente en Carretera") {
+		controlador(false, false, true, true, false);
+	} else if ($("#category option:selected").text() === "Inundación") {
+		controlador(true, true, true, false, false);
+	} else if ($("#category option:selected").text() === "Accidente Aéreo") {
+		controlador(false, false, false, false, false);
+	} else if ($("#category option:selected").text() === "Incendio") {
+		controlador(true, true, true, true, false);
+	} else if ($("#category option:selected").text() === "Meteorología") {
+		controlador(true, false, false, false, false);
+	} else if ($("#category option:selected").text() === "Otro") {
+		controlador(false, false, false, false, false);
+	}
+}
+
+function controlador(vCircu, vViento, presion, humedad, temperatura) {
 	$("#drivinVelocity").attr('disabled', vCircu);
 	$("#windVelocity").attr('disabled', vViento);
 	$("#preasure").attr('disabled', presion);
 	$("#humedad").attr('disabled', humedad);
 	$("#temperature").attr('disabled', temperatura);
 }
-
-$(document).ready(function(){
-	$("#category").change(function(){
-		if ($("#category option:selected").text()==="Otro"){
-			$("#tags").attr('disabled', false).focus();
-		} else {
-			$("#tags").attr('disabled', true);
-		}
-		if ($("#category option:selected").text()==="Accidente en Carretera"){
-			controlador(true, false, false, false, false);
-		}
-	})
-}, 
-function controlador(boolean vCircu, boolean vViento, 
-		boolean presion, boolean humedad, boolean temperatura){
-	$("#drivinVelocity").attr('disabled', vCircu);
-	$("#windVelocity").attr('disabled', vViento);
-	$("#preasure").attr('disabled', presion);
-	$("#humedad").attr('disabled', humedad);
-	$("#temperature").attr('disabled', temperatura);
-});
