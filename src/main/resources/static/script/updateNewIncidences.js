b2ce18e9c97d4ce752f489bda8bf3b4abf1a7d12
saulcasti <saul_casti@hotmail.es>
2018-03-27 19:02:59 +0200

    Entrega constante de peticiones post para recibir incidencias
    Se ha creado un js añadido al fragmento head.

---
+++ src/main/resources/static/script/updateNewIncidences.js
@@ -0,0 +1,10 @@
+window.setInterval(
+    function(){
+    // Sección de código para modificar el DIV
+    // $("#miDiv").text(variable);
+
+    // Ejemplo: Cada dos segundos se imprime la hora
+    console.log(Date());
+  }
+  // Intervalo de tiempo
+,2000);
