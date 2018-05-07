f7bdae2a41af48ea78622933f845b2b9ab3d5f5b
uo252473@uniovi.es <uo252473@uniovi.es>
2018-05-07 23:10:04 +0200

    sincronizacion con kafkacloud completa

--- src/main/java/uo/asw/kafka/producers/KafkaProducerConfig.java
+++ src/main/java/uo/asw/kafka/producers/KafkaProducerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,15 +20,19 @@ import uo.asw.dbManagement.model.Incidencia;
 @Configuration
 public class KafkaProducerConfig {
 	
-	@Value("${kafka.bootstrap-servers}")
-	private String bootstrapServer;
-	
 	@Bean
 	public ProducerFactory<String, Incidencia> producerFactory() {
-	    Map<String, Object> configProps = new HashMap<>();
-	    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-	    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-	    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		String usuario = "fsj71lf2";
+		String password = "1XUrUi1Zy9lopTAwx5q-Jho3UfkWSFZN";
+		String jaasConfig = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"" + usuario + "\" password=\"" + password + "\";";
+	    String btServer = "ark-01.srvs.cloudkafka.com:9094,ark-02.srvs.cloudkafka.com:9094,ark-03.srvs.cloudkafka.com:9094";
+		Map<String, Object> configProps = new HashMap<>();
+	    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, btServer);
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		configProps.put("security.protocol", "SASL_SSL");
+		configProps.put("sasl.mechanism", "SCRAM-SHA-256");
+		configProps.put("sasl.jaas.config", jaasConfig);
 	    return new DefaultKafkaProducerFactory<>(configProps);
 	}
  
